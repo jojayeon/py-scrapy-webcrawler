4873a08a2a3d086bc2faf3af776a41c600b47fcd
jojayeon <jojayeon6152@gmail.com>
2024-09-11 17:30:21 +0900

    주소같으면 가져오지 않게 설정

--- scrapy_crawler/scrapy_crawler/spiders/Continuous_Crawling_Spider.py
+++ scrapy_crawler/scrapy_crawler/spiders/Continuous_Crawling_Spider.py
@@ -1,10 +1,8 @@
 import scrapy
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin
 from konlpy.tag import Okt
 from collections import Counter
 import time
-import os
-
 
 class ContinuousCrawlingSpider(scrapy.Spider):
     name = "Continuous_Crawling_Spider"
@@ -17,7 +15,7 @@ class ContinuousCrawlingSpider(scrapy.Spider):
             f'https://search.hani.co.kr/search?searchword={search_term}',
             f'https://search.daum.net/search?w=news&lpp=10&DA=STC&rtmaxcoll=1&q={search_term}'
         ]
-
+        
         self.xpath_expressions = [
             '//p//text()[normalize-space()]',
             '//div//text()[normalize-space()]',
@@ -41,18 +39,7 @@ class ContinuousCrawlingSpider(scrapy.Spider):
         self.word_count = Counter()
         self.excluded_words = ["이", "가", "은", "는", "께", "서"]
         self.okt = Okt()
-        self.visited_sites = self.load_visited_sites()  # 방문한 URL 로드
-
-    def load_visited_sites(self):
-        if os.path.exists('visited_sites.txt'):
-            with open('visited_sites.txt', 'r', encoding='utf-8') as f:
-                return set(line.strip() for line in f)
-        return set()
-
-    def save_visited_sites(self):
-        with open('visited_sites.txt', 'w', encoding='utf-8') as f:
-            for url in self.visited_sites:
-                f.write(f'{url}\n')
+        self.last_save_time = time.time()  # 초기화 확인
 
     def is_valid_url(self, url):
         invalid_patterns = [
@@ -74,10 +61,7 @@ class ContinuousCrawlingSpider(scrapy.Spider):
         links = response.xpath('//a/@href').getall()
         for link in links:
             full_url = urljoin(response.url, link)
-            domain = urlparse(full_url).netloc
-
-            if full_url not in self.visited_sites and self.is_valid_url(full_url):
-                self.visited_sites.add(full_url)  # 이미 방문한 URL로 기록
+            if self.is_valid_url(full_url):
                 yield scrapy.Request(full_url, callback=self.parse_page)
 
     def parse_page(self, response):
@@ -89,17 +73,15 @@ class ContinuousCrawlingSpider(scrapy.Spider):
                     filtered_words = [word for word in words if word not in self.excluded_words]
                     self.word_count.update(filtered_words)
 
-        # 주기적으로 단어 개수를 파일에 기록
         current_time = time.time()
-        if current_time - self.last_save_time > 300:  # 5분마다 저장
+        if current_time - self.last_save_time > 3600:  # 5분마다 저장
             self.save_word_counts()
             self.last_save_time = current_time
 
     def save_word_counts(self):
         total_words = sum(self.word_count.values())
-        min_word_count = 30  # 원하는 갯수 이상의 단어만 보이게
-
-        with open('word_counts2s.txt', 'w', encoding='utf-8') as f:
+        min_word_count = 30
+        with open('word_counts2.txt', 'w', encoding='utf-8') as f:
             f.write(f'Total word count: {total_words}\n\n')
             for word, count in self.word_count.items():
                 if count >= min_word_count:
@@ -107,4 +89,3 @@ class ContinuousCrawlingSpider(scrapy.Spider):
 
     def closed(self, reason):
         self.save_word_counts()
-        self.save_visited_sites()
