85ff8390b941ea93a20fd88d40eaa6f9edb4f981
jojayeon <jojayeon6152@gmail.com>
2024-09-11 17:29:27 +0900

    1시간 주기로 스파이더 실행하는 파이썬 파일

--- run_continuous_crawler.py
+++ run_continuous_crawler.py
@@ -2,18 +2,32 @@ import os
 import subprocess
 import time
 
-def run_spider():
-    project_dir = 'scrapy_crawler'  # 스크래피 프로젝트 디렉토리 경로
+def run_spider(search_term):
+    project_dir = 'C:/Users/Administrator/py-scrapy-webcrawler/scrapy_crawler'  # 스크래피 프로젝트 디렉토리 경로
     
     while True:
         try:
             # 현재 디렉토리를 스크래피 프로젝트 디렉토리로 변경
             os.chdir(project_dir)
+            
             # 스크래피 명령어 실행
-            subprocess.run(["scrapy", "crawl", "Continuous_Crawling_Spider"], check=True, cwd=project_dir)
+            result = subprocess.run(
+                ["scrapy", "crawl", "Continuous_Crawling_Spider", "-a", f"search_term={search_term}"],
+                check=True,
+                capture_output=True,  # 명령어 출력 캡처
+                text=True  # 텍스트 형식으로 출력
+            )
+            
+            # 명령어 실행 결과 출력
+            print(result.stdout)
+            print(result.stderr)
+        
         except subprocess.CalledProcessError as e:
             print(f"Error occurred: {e}")
-        time.sleep(300)  # 5분 대기
+            print(f"Error output: {e.output}")
+        
+        time.sleep(3600)  # 1시간 대기 (단위: 초)
 
 if __name__ == "__main__":
-    run_spider()
+    search_term = "저출산"  # 원하는 검색어로 변경
+    run_spider(search_term)
